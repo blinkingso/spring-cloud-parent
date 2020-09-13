09a173c25fe3e4051ba92c690931f83775f0ed24
blinkingso <714232542@qq.com>
2020-09-13 22:36:28 +0800

    hystrix global configuration for per controller

--- cloud-consumer-hystrix-order80/src/main/kotlin/com/yz/order/controller/HystrixOrderPaymentController.kt
+++ cloud-consumer-hystrix-order80/src/main/kotlin/com/yz/order/controller/HystrixOrderPaymentController.kt
@@ -1,5 +1,9 @@
 package com.yz.order.controller
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty
+import com.yz.commons.entities.CommonResult
 import com.yz.order.service.OrderPaymentService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
@@ -14,6 +18,10 @@ import org.springframework.web.bind.annotation.RestController
  */
 @RestController
 @RequestMapping("/hystrix")
+// 全局的fallback方法, 需要在每个方法上添加注解@HystrixCommand
+@DefaultProperties(defaultFallback = "defaultGlobalFallback", commandProperties = [
+    HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2500")
+])
 class HystrixOrderPaymentController {
 
     @Autowired
@@ -25,7 +33,17 @@ class HystrixOrderPaymentController {
     }
 
     @GetMapping("/order/timeout/{id}")
+//    @HystrixCommand(fallbackMethod = "timeoutFallback", commandProperties = [HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000")])
+    @HystrixCommand
     fun timeout(@PathVariable("id") id: String): String {
         return orderPaymentService.timeout(id)
+    }
+
+    fun timeoutFallback(id: String): String {
+        return "客户端线程: " + Thread.currentThread().name + "调用超时, 请稍后重试吧...id 是 $id"
+    }
+
+    fun defaultGlobalFallback(): String {
+        return "请求出现了问题喽, 可能是超时喽, 客户端线程: ${Thread.currentThread().name}"
     }
 }
--- cloud-provider-hystrix-payment8003/src/main/kotlin/com/yz/payment/service/PaymentService.kt
+++ cloud-provider-hystrix-payment8003/src/main/kotlin/com/yz/payment/service/PaymentService.kt
@@ -18,13 +18,16 @@ class PaymentService {
     }
 
     @HystrixCommand(fallbackMethod = "timeoutFallback", commandProperties = [
-        HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000 ")
+        HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")
     ])
     fun timeout(id: String): String {
         TimeUnit.SECONDS.sleep(3)
         return "线程: ${Thread.currentThread().name}-> timeout(), id: $id, ~~~~~~哈哈, 耗时3s"
     }
 
+    /**
+     * 要求正常服务在2s内执行结束， 超过2s后服务降级返回
+     */
     fun timeoutFallback(id: String): String {
         return "线程: ${Thread.currentThread().name}-> timeoutFallback(), id: $id, ~~~~~~哈哈, 我是服务降级的功能啦"
     }
